6a36f137555dcc694b8daf3f128f0646b7c725c7
Davi Ribeiro <foxpc18@gmail.com>
2026-02-09 10:46:42 -0300

    Create firts component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import * as React from "react"
-import { OpenInV0Button } from "@/components/open-in-v0-button"
-import { HelloWorld } from "@/registry/new-york/blocks/hello-world/hello-world"
-import { ExampleForm } from "@/registry/new-york/blocks/example-form/example-form"
-import PokemonPage from "@/registry/new-york/blocks/complex-component/page"
-import { ExampleCard } from "@/registry/new-york/blocks/example-with-css/example-card"
+import * as React from 'react';
+import { OpenInV0Button } from '@/components/open-in-v0-button';
+import { HelloWorld } from '@/registry/new-york/blocks/hello-world/hello-world';
+import { ExampleForm } from '@/registry/new-york/blocks/example-form/example-form';
+import PokemonPage from '@/registry/new-york/blocks/complex-component/page';
+import { ExampleCard } from '@/registry/new-york/blocks/example-with-css/example-card';
+import { MyButton } from '@/registry/new-york/my-button/my-button';
 // This page displays items from the custom registry.
 // You are free to implement this with your own design as needed.
 
@@ -17,7 +18,18 @@ export default function Home() {
         </p>
       </header>
       <main className="flex flex-col flex-1 gap-8">
-        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
+        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-112.5 relative">
+          <div className="flex items-center justify-between">
+            <h2 className="text-sm text-muted-foreground sm:pl-3">
+              Meu primeiro botão
+            </h2>
+            <OpenInV0Button name="my-button" className="w-fit" />
+          </div>
+          <div className="flex items-center justify-center min-h-100 relative">
+            <MyButton variant="outline">Hello World</MyButton>
+          </div>
+        </div>
+        {/* <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
           <div className="flex items-center justify-between">
             <h2 className="text-sm text-muted-foreground sm:pl-3">
               A simple hello world component
@@ -63,8 +75,8 @@ export default function Home() {
           <div className="flex items-center justify-center min-h-[400px] relative">
             <ExampleCard />
           </div>
-        </div>
+        </div> */}
       </main>
     </div>
-  )
+  );
 }
